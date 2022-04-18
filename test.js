9a61a48801354c91882ef528899413e0b670fb67
Sebastian Miranda <sebastian_miranda@mckinsey.com>
2022-04-17 19:04:04 -0600

    modified test.js

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
 alert("All systems operational.")
 console.log("All systems operational.")
 
-console.error("Systems compromised. Critical error found.")
+let num = 2;
+
+if (num % 2 === 0){
+    console.log("Number is even!")
+}
+else {
+    console.log("Number is odd!")
+}
