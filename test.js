f6ca49de5d60a554367c0765de24b28bb9dee92b
SMG-1 <smirandagolfin75@gmail.com>
2022-04-18 20:22:04 -0600

    changes to test.js

--- test.js
+++ test.js
@@ -9,3 +9,12 @@ if (num % 2 === 0){
 else {
     console.log("Number is odd!")
 }
+
+let password = 'chicken Gal'
+
+if (password.length >= 8 && password.indexOf(' ') === -1){
+    console.log('Valid password')
+}
+else {
+    console.log('Invalid password')
+}
